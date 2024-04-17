3832ad75e384a02529abf983fb1c55c5c7fa7e93
takuyakowaki <68277608+takuyakowaki@users.noreply.github.com>
2024-04-18 02:39:41 +0900

    Add files via upload

--- gs-train/html_kowaki_06/js/main.js
+++ gs-train/html_kowaki_06/js/main.js
@@ -1,1 +1,13 @@
 // jsを記述する際はここに記載していく
+
+// scroll
+$(function(){
+    $('a[href^="#"]').click(function(){
+      var speed = 500;
+      var href= $(this).attr("href");
+      var target = $(href == "#" || href == "" ? 'html' : href);
+      var position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+    });
+  });
